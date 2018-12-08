155b4388241e6529b97024f5fc02b209a22bd521
Nick Bellizzi <ngb3@illinois.edu>
2018-12-08 16:43:11 -0600

    Moved API call to NewSongMenu (now working). Last thing to do is to properly wait until API call is finished to retrieve word.

--- app/src/main/java/com/example/nickb/songtranscriber/NewSongMenu.java
+++ app/src/main/java/com/example/nickb/songtranscriber/NewSongMenu.java
@@ -7,11 +7,44 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class NewSongMenu extends Activity {
+
+    private Map<String, String> previous;
+    private boolean callError;
+    private RequestQueue requestQueue;
+    private final String apiURL = "https://wordsapiv1.p.rapidapi.com/words/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_song_layout);
+        requestQueue = Volley.newRequestQueue(this);
+        loadPrevious();
+    }
+
+    private void loadPrevious() {
+        previous = new HashMap<>();
+        previous.put("isn't", "ˈɪzənt");
+        //do later
     }
 
     public void onSubmitSongInformation(View view) {
@@ -22,7 +55,9 @@ public class NewSongMenu extends Activity {
         String artistName = String.valueOf(artistText.getText());
         EditText lyricsText = (EditText) findViewById(R.id.input_lyrics);
         String songLyrics = String.valueOf(lyricsText.getText());
-        SongInfo newSong = new SongInfo(songTitle, artistName, songLyrics, this);
+        String[] lines = songLyrics.split("\\r?\\n");
+        String[] conversions = convertLines(lines);
+        SongInfo newSong = new SongInfo(songTitle, artistName, lines, conversions);
         Log.i(NewSongMenu.class.getName(), "sos " + newSong.toString());
         // handle errors
         Intent returnToMenu = new Intent();
@@ -31,5 +66,93 @@ public class NewSongMenu extends Activity {
         returnToMenu.putExtras(bundle);
         setResult(RESULT_OK, returnToMenu);
         finish();
+    }
+
+    private String[] convertLines(String[] lines) {
+        System.out.println("in convert lines");
+        String[] ipaTranscriptions = new String[lines.length];
+        for (int i = 0; i < lines.length; i++) {
+            ipaTranscriptions[i] = convertLine(lines[i].toLowerCase());
+        }
+        return ipaTranscriptions;
+    }
+
+    private String convertLine(String line) {
+        System.out.println("in convert line");
+        String[] words = line.trim().split(" ");
+        String transcribedLine = "";
+        for (String word : words) {
+            transcribedLine += convertWord(word) + " ";
+        }
+        return transcribedLine.trim();
+    }
+
+    private String convertWord(String word) {
+        if (previous.containsKey(word)) {
+            return previous.get(word);
+        }
+        System.out.println("in convert word");
+        callError = false;
+        //preprocess word??
+        startAPICall(word);
+        //while (!previous.containsKey(word) && !callError) {}
+        try {
+            Thread.sleep(900);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "ErRoR";
+        }
+        if (callError) {
+            return "ERROR";
+        }
+        System.out.println("BACK TO CONVERT WORDS");
+        return previous.get(word);
+    }
+
+    private void startAPICall(final String word) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    apiURL + word,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println(word);
+                            processResponse(response, word);
+                            System.out.println("got json response!");
+                            System.out.println(word + " : " + previous.get(word));
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println("api error! " + error.toString());
+                            callError = true;
+                        }
+                    }
+            ) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String>  params = new HashMap<>();
+                    params.put("X-RapidAPI-Key", "e578e68f77msheb452bc0de6aa22p186f2cjsn504e4028958e");
+                    return params;
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processResponse(JSONObject response, String word) {
+        try {
+            System.out.println("successful api response?");
+            String ipaWord = response.getJSONObject("pronunciation").getString("all");
+            previous.put(word, ipaWord);
+        } catch (JSONException e) {
+            System.out.println("got word, didn't parse correctly");
+            previous.put(word, "ERROR PARSING");
+        }
     }
 }
--- app/src/main/java/com/example/nickb/songtranscriber/SongInfo.java
+++ app/src/main/java/com/example/nickb/songtranscriber/SongInfo.java
@@ -1,7 +1,6 @@
 package com.example.nickb.songtranscriber;
 
 import android.content.Context;
-import android.view.textclassifier.TextLinks;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -9,10 +8,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -23,33 +27,30 @@ import org.json.JSONObject;
 public class SongInfo implements Serializable {
     private String title;
     private String artist;
-    private String lyrics;
     private String[] lines;
     private String[] ipaTranscriptions;
     private long[] lineTimings;
     private Map<String, String> previous;
 
-    private RequestQueue requestQueue;
+    /*private RequestQueue requestQueue;
     private final String apiURL = "https://wordsapiv1.p.rapidapi.com/words/";
+    private JSONObject jsonObject;*/
 
-    SongInfo(String title, String artist, String lyrics, Context ctx) {
+    SongInfo(String title, String artist, String[] lyrics, String[] conversions) {
         this.title = title;
         this.artist = artist;
-        this.lyrics = lyrics;
-        lines = lyrics.split("\\r?\\n");
-        requestQueue = Volley.newRequestQueue(ctx);
-        loadPrevious();
-        convertLines();
-        System.out.println(Arrays.toString(ipaTranscriptions));
+        this.lines = lyrics;
+        this.ipaTranscriptions = conversions;
+        System.out.println("IPA Transcriptions: " + Arrays.toString(ipaTranscriptions));
     }
 
-    private void convertLines() {
-        /*ipaTranscriptions = new String[]{
+    /*private void convertLines() {
+        ipaTranscriptions = new String[]{
                 "aɪm ˈlʊkɪŋ ˈoʊvər ə fɔr-lif ˈkloʊvər",
                 "aɪ ˈoʊvərˌlʊkt bɪˈfɔr",
                 "wʌn lif ɪz ˈsʌnˌʃaɪn, ðə ˈsɛkənd ɪz reɪn",
                 "θɜrd ɪz ðə ˈroʊzɪz ðæt groʊ ɪn ðə leɪn"
-        };*/
+        };
         System.out.println("in convert lines");
         for (int i = 0; i < lines.length; i++) {
             ipaTranscriptions[i] = convertLine(lines[i]);
@@ -60,8 +61,8 @@ public class SongInfo implements Serializable {
         System.out.println("in convert line");
         String[] words = line.trim().split(" ");
         String transcribedLine = "";
-        for (int i = 0; i < words.length; i++) {
-            transcribedLine += convertWord(words[i]) + " ";
+        for (String word : words) {
+            transcribedLine += convertWord(word) + " ";
         }
         return transcribedLine.trim();
     }
@@ -74,47 +75,55 @@ public class SongInfo implements Serializable {
         }
         String simple = word; // preprocessed word?
         try {
-            String apiResult = startAPICall(simple);
-            System.out.println("api call");
-            if (!apiResult.equals("ERROR")) {
-                return apiResult;
+            startAPICall(simple);
+            System.out.println("api call finished");
+            if (jsonObject != null) {
+                try {
+                    System.out.println("successful api response?");
+                    return jsonObject.getJSONObject("pronunciation").getString("all");
+                } catch (JSONException e) {
+                    System.out.println("got word, didn't parse correctly");
+                    return "ERROR PARSING";
+                } catch (NullPointerException e) {
+                    System.out.println("json null!");
+                    return "json null!";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            return word;
+            return "other error";
         } catch (Exception e) {
-            System.out.println("exception");
+            System.out.println("exception" + e.toString());
+            e.printStackTrace();
             return word;
         }
     }
 
-    private String startAPICall(String simple) {
+    private void startAPICall(final String simple) {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    apiURL + simple + "/pronunciation",
+                    apiURL + simple,
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            try {
-                                String wordResult = response.getJSONObject("pronunciation").getString("all");
-                            } catch (JSONException e) {
-                                System.out.println("got word, didn't parse correctly");
-                            }
-                            requestQueue.stop();
+                            System.out.println(simple);
+                            System.out.println("got json response!");
+                            jsonObject = response;
                         }
                     },
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
                             System.out.println("api error! " + error.toString());
-                            requestQueue.stop();
                         }
                     }
             ) {
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String>  params = new HashMap<String, String>();
-                    params.put("X-RapidAPI-Key", "apiKey");
+                    Map<String, String>  params = new HashMap<>();
+                    params.put("X-RapidAPI-Key", "e578e68f77msheb452bc0de6aa22p186f2cjsn504e4028958e");
                     return params;
                 }
             };
@@ -122,13 +131,12 @@ public class SongInfo implements Serializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "ERROR";
     }
 
     private void loadPrevious() {
         previous = new HashMap<>();
         previous.put("isn't", "ˈɪzənt"); //do later
-    }
+    }*/
 
     public String[] getLines() {
         return lines;
